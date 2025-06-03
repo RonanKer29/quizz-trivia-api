72f23c4180ca56d53f4d3e724c895b95fb9dce52
RonanKer29 <kervella.ronan@hotmail.com>
2025-06-03 12:11:26 +0200

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./components/Login";
 import Leaderboard from "./components/Leaderboard";
 import { getUser, signOut } from "./utils/auth";
 import { CATEGORY_LABELS } from "./utils/categories";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -105,6 +106,7 @@ const App = () => {
         />
         <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
+      <Analytics />
     </div>
   );
 };
