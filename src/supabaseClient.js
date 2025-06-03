2dc15acdb2c0064b40d05371f0dcc78b1ae39f3b
RonanKer29 <kervella.ronan@hotmail.com>
2025-06-03 10:29:10 +0200

    env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mqnbaczlrcdkzjhcfxjr.supabase.co"; // récupère ça sur ton dashboard
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbmJhY3pscmNka3pqaGNmeGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5MjM3MTMsImV4cCI6MjA2NDQ5OTcxM30.T9_8YoeZ-8tDXw7FWSLcN6dqk1GVBVonkR8u5hLEOVo"; // prends le `anon` public key (non secret)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
